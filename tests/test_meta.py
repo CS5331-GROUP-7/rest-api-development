593cc8b8addeb34ea90356120ee9be66978dcb35
Tan Xue Si <tanxs93@gmail.com>
2018-03-03 20:34:30 +0800

    add meta endpoint tests

---
+++ tests/test_meta.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+from flask import url_for
+
+
+def test_meta_heartbeat(client):
+    page = client.get(url_for('meta_heartbeat'))
+    assert page.status_code == 200
+
+    data = json.loads(page.data)
+    assert 'status' in data
+    assert data['status']
+
+
+def test_meta_members(client):
+    page = client.get(url_for('meta_members'))
+    assert page.status_code == 200
+
+    data = json.loads(page.data)
+    assert 'status' in data
+    assert 'result' in data
+
+    assert data['status']
+    assert data['result'] == ['Zawlin', 'Xue Si', 'ShiQing', 'ChenHui']
+
+
+if __name__ == '__main__':
+    pytest.main()
