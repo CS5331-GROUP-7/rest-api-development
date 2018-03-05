aa57ed4bae40e9d868881a5a0f1a6617bc917887
zawlin <kzl.zawlin@gmail.com>
2018-03-05 19:50:08 +0800

    * fix view problems

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,7 @@
 import json
-from flask import url_for
 
 def send_post_data(client, endpoint, data):
-    return client.post(url_for(endpoint),
+    return client.post(endpoint,
                 data=json.dumps(data),
                 content_type='application/json',
                 environ_base={'REMOTE_ADDR': '127.0.0.1'})
