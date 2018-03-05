7606c7f70f427af41ac80333695df4740e3eca92
zawlin <kzl.zawlin@gmail.com>
2018-03-05 19:43:28 +0800

    * unify send_post_data

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,19 +2,13 @@ import pytest
 import json
 from src.service.app import create_app
 from src.service.app import app as flask_app
-from flask import url_for
 from flask_mongoengine import MongoEngine
 from mongoengine import connect
 
 
-def send_post_data(client, endpoint, data):
-    client.post(url_for(endpoint),
-                data=json.dumps(data),
-                content_type='application/json',
-                environ_base={'REMOTE_ADDR': '127.0.0.1'})
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture#(scope='session')
 def app():
 
     # db = connect('mongodb')
